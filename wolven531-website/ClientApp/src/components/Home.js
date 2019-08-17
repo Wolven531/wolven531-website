8eacbdece2058b9ff0cc31cef30fbbc1a2c5ed73
Anthony Williams <anthony.williams.cs@gmail.com>
2019-08-16 23:27:45 -0600

    Add class name for targeting

--- wolven531-website/ClientApp/src/components/Home.js
+++ wolven531-website/ClientApp/src/components/Home.js
@@ -7,7 +7,7 @@ export class Home extends Component {
 
     render() {
         return (
-            <div>
+            <div className="home">
                 <h1>Wolven531</h1>
                 <p>Welcome to Anthony Williams' personal website</p>
                 <p>It is still under [<a href="https://github.com/Wolven531/wolven531-website" target="_blank" rel="noopener noreferrer">active</a>] construction</p>
