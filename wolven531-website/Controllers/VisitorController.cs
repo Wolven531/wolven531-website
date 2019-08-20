41a9440648d4674cde43ffa90b03c76d7e3c155a
Anthony Williams <anthony.williams.cs@gmail.com>
2019-08-19 22:56:44 -0600

    Add first pass at VisitorController

---
+++ wolven531-website/Controllers/VisitorController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using wolven531_website.Services;
+
+namespace wolven531_website.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VisitorController : ControllerBase
+    {
+        private readonly IVisitorService _visitorService;
+        private readonly ILogger<VisitorController> _logger;
+
+        public VisitorController(IVisitorService visitorService, ILogger<VisitorController> logger)
+        {
+            _visitorService = visitorService;
+            _logger = logger;
+        }
+
+        // GET: api/Visitor
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        //// GET: api/Visitor/5
+        //[HttpGet("{id}", Name = "Get")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST: api/Visitor
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT: api/Visitor/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
