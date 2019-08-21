b7646aaf4eb3173960ad5e6485c091d6f40e9341
Anthony Williams <anthony.williams.cs@gmail.com>
2019-08-20 22:58:46 -0600

    Render visitor list

--- wolven531-website/ClientApp/src/components/Visitors/Visitors.jsx
+++ wolven531-website/ClientApp/src/components/Visitors/Visitors.jsx
@@ -1,56 +1,51 @@
 import React, { Component } from 'react'
 
 class Visitors extends Component {
+	state = {
+		isLoading: true,
+		visitors: []
+	}
+
 	componentDidMount() {
 		window.document.title = 'Visitors - Wolven531'
-		// let results = ''
+
 		fetch('api/visitors')
 			.then(resp => {
 				if (resp.status !== 200) {
 					console.log(`Response status was not 200; instead status=${resp.status}`)
 					return
 				}
-				console.log(resp)
-				return resp.text()
-				// return resp.body
-				// const streamReader = resp.body.getReader()
-				// streamReader.read()
-				// 	.then()
-				// 	.catch()
-
-				// return resp.json()
+				// console.log(resp)
+				return resp.json() // resp.text()
 			})
-			.then(respText => {
-				console.log(respText)
+			// .then(respText => {
+			// 	console.log(respText)
+			// })
+			.then(visitors => {
+				this.setState({ isLoading: false, visitors })
 			})
-			// .then(responseStream => {
-			// 	// const textStream = new TextEncoderStream()
-			// 	// responseStream.pipeThrough(textStream)
-			// 	// return textStream.readable
-			// 	responseStream.getReader().read().then(({ done, value }) => {
-			// 		if (done) {
-			// 			responseStream.getReader().releaseLock()
-			// 			console.log(results)
-			// 			return
-			// 		}
-			// 		results += value
-			// 	})
-			// })
-			//// .then(readableStream => {
-			//// 	console.log(readableStream)
-			//// })
-			// .then(respJson => {
-			// 	console.log(respJson)
-			// })
 			.catch(err => {
 				console.error(err)
 			})
 	}
 
 	render() {
+		const { isLoading, visitors } = this.state
+
 		return (
 			<div>
 				<h2>Visitors</h2>
+				{isLoading
+					? <p>Loading...</p>
+					: <div>
+						<h3>Visitor List ({visitors.length})</h3>
+						<ul>
+							{visitors.map(visitor =>
+								<li>{visitor}</li>)
+							}
+						</ul>
+					</div>
+				}
 			</div>
 		)
 	}
