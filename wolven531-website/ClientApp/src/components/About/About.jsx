b14c9ded67881cb779776c0e731dc5ff2ba41d90
Anthony Williams <anthony.williams.cs@gmail.com>
2019-08-22 23:38:50 -0600

    Add skeleton for About component

---
+++ wolven531-website/ClientApp/src/components/About/About.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import './About.css'
+
+class About extends Component {
+	componentDidMount() {
+		window.document.title = 'About - Wolven531'
+	}
+
+	render() {
+		return (
+			<div className="about">
+				
+			</div>
+		)
+	}
+}
+
+export { About }
