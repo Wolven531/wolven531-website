c3bb8d4cd8ac5e7bd823aef02d15c9742e2aefcd
Anthony Williams <anthony.williams.cs@gmail.com>
2019-08-23 22:11:46 -0600

    Clean up client POST code

--- wolven531-website/ClientApp/src/components/Visitors/Visitors.jsx
+++ wolven531-website/ClientApp/src/components/Visitors/Visitors.jsx
@@ -9,6 +9,7 @@ class Visitors extends Component {
 
 	handleRegistration = () => {
 		fetch('api/visitors', {
+			body: JSON.stringify({ name: this.state.visitorName }),
 			// cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
 			// credentials: 'same-origin', // include, *same-origin, omit
 			method: 'POST', // *GET, POST, PUT, DELETE, etc.
@@ -17,13 +18,9 @@ class Visitors extends Component {
 				// 'Content-Type': 'application/json'
 				'Content-Type': 'application/json; charset=utf-8'
 				// 'Content-Type': 'application/x-www-form-urlencoded',
-			},
+			}
 			// redirect: 'follow', // manual, *follow, error
 			// referrer: 'no-referrer', // no-referrer, *client
-			// body: JSON.stringify({ visitorName: this.state.visitorName }) // body data type must match "Content-Type" header
-			body: JSON.stringify({ name: this.state.visitorName })
-			// body: '"' + JSON.stringify({ visitorName: this.state.visitorName }).replace(/"/gi, "'") + '"'
-			// body: `"${this.state.visitorName}"`
 		})
 		.then(resp => {
 			console.log(`posted and got response status = ${resp.status}`)
