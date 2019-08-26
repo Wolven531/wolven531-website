290b7177a9f75ad92a9599477ef785813108e48b
Anthony Williams <anthony.williams.cs@gmail.com>
2019-08-25 23:53:08 -0600

    Add addVisitor to Api

--- wolven531-website/ClientApp/src/api.js
+++ wolven531-website/ClientApp/src/api.js
@@ -1,4 +1,31 @@
 class Api {
+	addVisitor = visitorName => {
+		return fetch('api/visitors', {
+			body: JSON.stringify({ name: visitorName }),
+			// body: JSON.stringify({ name: this.state.visitorName }),
+			// cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+			// credentials: 'same-origin', // include, *same-origin, omit
+			method: 'POST', // *GET, POST, PUT, DELETE, etc.
+			// mode: 'cors', // no-cors, cors, *same-origin
+			headers: {
+				'Content-Type': 'application/json; charset=utf-8'
+				// 'Content-Type': 'application/x-www-form-urlencoded'
+			}
+			// redirect: 'follow', // manual, *follow, error
+			// referrer: 'no-referrer', // no-referrer, *client
+		})
+			.then(resp => resp.json())
+			.then(respJson => {
+				if (respJson.error) {
+					throw new Error(`Poorly formatted request. Error:\n\n${respJson.error}`)
+				}
+				return respJson
+			})
+			.catch(err => {
+				alert(err)
+			})
+	}
+
 	getVisitors = () => {
 		return fetch('api/visitors')
 			.then(resp => {
--- wolven531-website/ClientApp/src/components/Visitors/Visitors.jsx
+++ wolven531-website/ClientApp/src/components/Visitors/Visitors.jsx
@@ -24,31 +24,13 @@ class Visitors extends Component {
 	}
 
 	handleRegistration = () => {
-		fetch('api/visitors', {
-			body: JSON.stringify({ name: this.state.visitorName }),
-			// cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-			// credentials: 'same-origin', // include, *same-origin, omit
-			method: 'POST', // *GET, POST, PUT, DELETE, etc.
-			// mode: 'cors', // no-cors, cors, *same-origin
-			headers: {
-				'Content-Type': 'application/json; charset=utf-8'
-				// 'Content-Type': 'application/x-www-form-urlencoded'
-			}
-			// redirect: 'follow', // manual, *follow, error
-			// referrer: 'no-referrer', // no-referrer, *client
-		})
-			.then(resp => resp.json())
+		const visitorName = this.state.visitorName
+		this.api.addVisitor(visitorName)
 			.then(respJson => {
-				if (respJson.error) {
-					throw new Error(`Poorly formatted request. Error:\n\n${respJson.error}`)
-				}
 				this.setState({
 					visitorName: '',
 					visitors: this.state.visitors.concat(this.state.visitorName)
 				})
-			})
-			.catch(err => {
-				alert(err)
 			})
 	}
 
