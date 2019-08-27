69764fa7e7face25828b6a143a8e0f913e8c773c
Anthony Williams <anthony.williams.cs@gmail.com>
2019-08-26 22:54:55 -0600

    Fix imports in App

--- wolven531-website/ClientApp/src/components/App/App.jsx
+++ wolven531-website/ClientApp/src/components/App/App.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router'
 
-import { About } from '../About/About'
-import { Counter } from '../Counter'
-import { FetchData } from '../FetchData'
+// views
+import { About } from '../../views/About'
+import { Counter } from '../../views/Counter'
+import { FetchData } from '../../views/FetchData'
+import { Visitors } from '../../views/Visitors'
+
+// components
 import { Home } from '../Home/Home'
 import { Layout } from '../Layout/Layout'
-import { Visitors } from '../Visitors/Visitors'
 
 class App extends Component {
 	render() {
