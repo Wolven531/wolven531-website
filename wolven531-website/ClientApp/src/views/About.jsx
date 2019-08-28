0c63f6ad8ccc50d4b12ec12f861886c9006d841b
Anthony Williams <anthony.williams.cs@gmail.com>
2019-08-27 23:18:49 -0600

    Add first pass at skill table

--- wolven531-website/ClientApp/src/views/About.jsx
+++ wolven531-website/ClientApp/src/views/About.jsx
@@ -10,6 +10,16 @@ class About extends Component {
 	render() {
 		return (
 			<div className="about">
+				<table>
+					<tr>
+						<th>Language</th>
+						<th>Skill Level</th>
+					</tr>
+					<tr>
+						<td>C#</td>
+						<td>4 of 5</td>
+					</tr>
+				</table>
 				<p>This website is designed using dotnet core (C#), TypeScript, and React</p>
 				<p>&copy; Anthony Williams 2019</p>
 			</div>
