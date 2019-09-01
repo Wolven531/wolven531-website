fb68d1cd55cf695e08946f0995406c3453a14ab5
Anthony Williams <anthony.williams.cs@gmail.com>
2019-08-31 23:26:48 -0600

    Fix semantic issue in About

--- wolven531-website/ClientApp/src/views/About.jsx
+++ wolven531-website/ClientApp/src/views/About.jsx
@@ -9,22 +9,26 @@ class About extends Component {
 		return (
 			<div className="about">
 				<table>
-					<tr>
-						<th>Language</th>
-						<th>Skill Level</th>
-					</tr>
-					<tr>
-						<td>C#</td>
-						<td>4 of 5</td>
-					</tr>
-					<tr>
-						<td>JavaScript</td>
-						<td>5 of 5</td>
-					</tr>
-					<tr>
-						<td>TypeScript</td>
-						<td>5 of 5</td>
-					</tr>
+					<thead>
+						<tr>
+							<th>Language</th>
+							<th>Skill Level</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>C#</td>
+							<td>4 of 5</td>
+						</tr>
+						<tr>
+							<td>JavaScript</td>
+							<td>5 of 5</td>
+						</tr>
+						<tr>
+							<td>TypeScript</td>
+							<td>5 of 5</td>
+						</tr>
+					</tbody>
 				</table>
 				<p>This website is designed using dotnet core (C#), TypeScript, and React</p>
 				<p>&copy; Anthony Williams 2019</p>
