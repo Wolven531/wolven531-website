b297fa4c3801754e68ca0bbc81becf95a72df882
Anthony Williams <anthony.williams.cs@gmail.com>
2019-09-04 22:54:27 -0600

    Introduce rotating pyramid

--- wolven531-website/ClientApp/src/views/Counter.tsx
+++ wolven531-website/ClientApp/src/views/Counter.tsx
@@ -2,12 +2,32 @@ import React, { Component } from 'react'
 
 interface ICounterState {
 	currentCount: number
+	currentYRotation: number
 }
 
 export class Counter extends Component<{}, ICounterState> {
+	rotationInterval: NodeJS.Timeout | null
+
 	constructor(props: any) {
 		super(props)
-		this.state = { currentCount: 0 }
+		this.state = {
+			currentCount: 0,
+			currentYRotation: 0
+		}
+		this.rotationInterval = null
+	}
+
+	public componentDidMount() {
+		this.rotationInterval = setInterval(() => {
+			this.setState({ currentYRotation: (this.state.currentYRotation + 1) % 360 })
+		}, 1000 / 60)
+	}
+
+	public componentWillUnmount() {
+		if (this.rotationInterval === null) {
+			return
+		}
+		clearInterval(this.rotationInterval)
 	}
 
 	incrementCounter = () => {
@@ -20,6 +40,11 @@ export class Counter extends Component<{}, ICounterState> {
 		return (
 			<div>
 				<h1>Counter</h1>
+				<img
+					src="/pyramid.svg" alt="Pyramid SVG"
+					style={{
+						transform: `rotate3d(0, 1, 0, ${this.state.currentYRotation}deg)`
+					}} />
 				<p>Current count: <strong>{this.state.currentCount}</strong></p>
 				<button onClick={this.incrementCounter}>Increment</button>
 			</div>
